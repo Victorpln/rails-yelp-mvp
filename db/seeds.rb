e36087e3e17183a14355ede41022f77a0d3bf6c6
victor plantevin <victor.plantevin@gmail.com>
2021-12-14 21:35:40 +0100

    exercice 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,24 @@
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'chinese',
+            phone_number: '06.78.74.07.64' }
+mamounia = { name: 'Mamounia', address: '7 Boundary St, London E2 7JE', category: 'chinese',
+             phone_number: '06.78.74.07.64' }
+mimoun = { name: 'Mimoun', address: '7 Boundary St, London E2 7JE', category: 'chinese',
+           phone_number: '06.78.74.07.64' }
+madiba = { name: 'Madiba', address: '7 Boundary St, London E2 7JE', category: 'chinese',
+           phone_number: '06.78.74.07.64' }
+makram = { name: 'Makram', address: '7 Boundary St, London E2 7JE', category: 'chinese',
+           phone_number: '06.78.74.07.64' }
+
+[dishoom, mamounia, mimoun, madiba, makram].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
